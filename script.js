a5f2ba916176f94c2de99e85691c94a8eeca8ada
Chidori <chidung200150@gmail.com>
2023-01-04 20:43:18 +0700

    second commit

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+let dot = ["dot1", "dot2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no]= n, no);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("indicator");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dot[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+     x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].classList.add(`active`);
-}
-
-var slideIndex = 1;
-showDivs2(slideIndex);
-
-function plusDivs(n) {
-  showDivs2(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs2(slideIndex = n);
-}
-function showDivs2(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides-2");
-  var dots = document.getElementsByClassName("indicator-2");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].classList.add(`active`);
+  x[slideIndex[no]-1].style.display = "block";  
+  dots[slideIndex[no]-1].className += " active";
 }
