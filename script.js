95f69b1d640bf334803593101a9ced5d1d7fa54f
Chidori <chidung200150@gmail.com>
2023-01-04 19:05:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("indicator");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].classList.add(`active`);
+}
+
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs(n) {
+  showDivs2(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs2(slideIndex = n);
+}
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides-2");
+  var dots = document.getElementsByClassName("indicator-2");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].classList.add(`active`);
+}
